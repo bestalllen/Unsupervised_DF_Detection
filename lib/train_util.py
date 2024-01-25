70e135e1677fdf8ce7e10d7ba76f08e6e50d5ca7
Best-Allen-always <62170857+bestalllen@users.noreply.github.com>
2024-01-25 21:20:01 +0800

    Add files via upload

---
+++ lib/train_util.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import torch
+import numpy as np
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def save_model(model, optimizer, opt, epoch, save_file):
+    print('==> Saving...')
+    state = {
+        'opt': opt,
+        'model': model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+        'epoch': epoch,
+    }
+    torch.save(state, save_file)
+    del state
